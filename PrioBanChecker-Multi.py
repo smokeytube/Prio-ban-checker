e165353adcfe3fbaebe5675e9ccf0a3b17cdae07
BGP0 <59474088+BGP0@users.noreply.github.com>
2020-11-01 18:37:30 +0000

    Added multithreading and window name stuff

--- PrioBanChecker-Multi.py
+++ PrioBanChecker-Multi.py
@@ -1,5 +1,11 @@
 import os.path
 import requests
+import threading
+import random
+import time
+import ctypes
+
+lines = open('usernames.txt').read().splitlines()
 
 title = """
 ════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
@@ -13,9 +19,19 @@ title = """
 ════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 """
 
+Banned = 0
+Unbanned = 0
+Finished = 0
+LastTotal = 0
+
+def random_line():
+    return random.choice(lines)
+
 def check(input):
+    global Banned
+    global Unbanned
     if len(input) < 3:
-        print('\nUnable to find a player with that username')
+        print(f'\nUnable to find {input}')
     else:
         data = f'ign={input}'
         headers = {
@@ -27,23 +43,45 @@ def check(input):
             print((f"{input} is currently banned").center(119))
             line = f'{input}\n'
             open('checked_usernames.txt', 'a').write(line)
+            Banned += 1
         else:
             print((f"{input} is not currently banned").center(119))
+            Unbanned += 1
 
-if os.path.exists('usernames.txt'):
-    open('checked_usernames.txt', 'w').close()
-    print(title)
-    print(('By BGP#0419\n'.center(119)))
-    file = open('usernames.txt', 'r')
+def start():
     while 0 == 0:
-        username = file.readline().replace('\n', '')
-        if username == '':
-            print(('Checked all usernames!').center(119))
+        usernames = open('usernames.txt').read()
+        if usernames == '':
             break
         else:
+            username = random_line()
+            open('usernames.txt', 'w').write(usernames.replace(f"\n{username}", '').replace(username, ''))
             check(username)
-    input('Press enter to close')
-else:
-    open('usernames.txt', 'a')
-    print('Put a list of usernames in "usernames.txt", then run the program again')
-    input('Press enter to close')
+
+def name():
+    global Finished
+    global LastTotal
+    while Finished == 0:
+        Speed = (((Banned + Unbanned) - LastTotal) / 3).__round__(2)
+        ctypes.windll.kernel32.SetConsoleTitleW(f"BGP's Prio Ban Checker | Banned: {Banned} | Unbanned: {Unbanned} | Speed: {Speed}/s")
+        LastTotal = Banned + Unbanned
+        time.sleep(3)
+
+print(title)
+print(('By BGP#0419\n'.center(119)))
+
+threads = []
+open('checked_usernames.txt', 'w').write('')
+numberofthreads = int(input('Enter amount of threads: '))
+print('Loading threads...')
+threading.Thread(target=name).start()
+for i in range(numberofthreads):
+    t = threading.Thread(target=start)
+    threads.append(t)
+    t.start()
+    time.sleep(.1)
+print(('\nFinished loading all threads.\n').center(119))
+for x in threads:
+    x.join()
+Finished = 1
+input(('Finished Checking! Press enter to close.').center(119))
